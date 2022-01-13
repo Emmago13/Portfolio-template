b86cbf938ac3fd925aa38406fd00450a9cb1f489
Emmanuel Gomez <emmagomez166@gmail.com>
2022-01-12 21:12:46 -0300

    Desktop view finished

---
+++ main.js
@@ -0,0 +1,7 @@
+var contactButton = document.getElementById('contactButton');
+
+contactButton.addEventListener('click',() =>{
+    window.location.href = './_contact.html';
+});
+
+console.log(contactButton);
