d076d9bfd0e7014919aba6e0d9b308a47fdaec4e
Emmanuel Gomez <emmagomez166@gmail.com>
2022-02-24 23:32:08 -0300

    Chores
    Changes in the navbar

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-// let contactButton = document.getElementById('contactButton');
 
-// contactButton.addEventListener('click',() =>{
-//     window.location.href = './_contact.html';
-// });
+const navbarMenuIcon = (() => {
+    let body = document.querySelector('body');
+    let menuIcon = document.querySelector('.menu__icon');
 
-// console.log(contactButton);
+    menuIcon.addEventListener('click', () => toogleClass(body, 'nav__active'));
+    
+    const toogleClass = (element, activeClass) => {
+        if (element.classList.contains(activeClass)) {
+            console.log(element);
+            element.classList.remove(activeClass);
+        }else{
+            console.log(element);
+            element.classList.add(activeClass);
+        }
+    }
 
-// console.log("Hello world");
-// let inputName = document.getElementById("inputName");
-
-// console.log(inputName);
-
-// let home = document.getElementById('home')
-// console.log(home);
-
-// let worksImage = document.getElementById('works--img');
-// let imgSpan = document.getElementById("img--span");
-// let imgHoover = false;
-// // console.log(imgSpan);
-
-// worksImage.addEventListener('mouseover',()=>{
-//     // imgSpan.classList.toggle("visible")
-//     if(!imgHoover){
-//         imgSpan.classList.add("visible");
-//         imgHoover = true;
-//     }else{
-//         imgSpan.classList.remove("visible");
-//         imgHoover = false;
-//     }
-// })
+})();
+ 
